dade2508968969b0b3c3a41bb665aa392bd65282
salvob02 <134782597+salvob02@users.noreply.github.com>
2023-06-16 15:23:18 +0200

    Update prova.py

--- prova.py
+++ prova.py
@@ -5,9 +5,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def root():
-    print("ciao") 
-    return "Hello World (Python)! (up %s)\n" 
+ 
+    return "Hello World (Python)!\n" 
     
 
 if __name__ == "__main__":
