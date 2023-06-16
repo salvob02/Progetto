cae978c0eecce0969d936dfbbb834f2083f9f8aa
salvob02 <134782597+salvob02@users.noreply.github.com>
2023-06-16 16:18:06 +0200

    Update prova.py

--- prova.py
+++ prova.py
@@ -1,17 +1,13 @@
-from flask import Flask, request, Response
 
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''}, methods=['GET', 'POST'])
-@app.route('/<path:path>', methods=['GET', 'POST'])
-def catch_all(path):
-        if request.form:
-                print('Form:' + str(request.form))
-        if request.data:
-                print('Data:' + str(request.data))
 
-        return ''
-    
+@app.route('/')
+def root():
+    i=0
+    return "Hello World (Python)! %d",i
+
             
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8081)
