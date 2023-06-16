8265acfaca9d6400aa1f9c1ff8e338e7e4322479
salvob02 <134782597+salvob02@users.noreply.github.com>
2023-06-16 16:07:38 +0200

    Update prova.py

--- prova.py
+++ prova.py
@@ -1,23 +1,12 @@
-#!/usr/bin/python
-
-
 from flask import Flask
-from sys import stderr
+
 app = Flask(__name__)
 
 @app.route('/')
 
 def root():
-    print('Hello world!', file=sys.stderr)
+    return 'Hello World!'
     
             
-               
-    
-
-    
-
-
-    
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8081)
